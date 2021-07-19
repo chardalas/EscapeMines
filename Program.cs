76566fa2f307f4e1a6ffc0b7b5203a2d9f352b42
chardalas <emmhard@gmail.com>
2021-07-19 11:50:27 +0200

    Rename dir

--- Program.cs
+++ Program.cs
@@ -4,53 +4,55 @@ using System.Linq;
 
 namespace EscapeMinesChardalasEmmanouil
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Welcome to the Escape Mines.\n");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Escape Mines.\n");
 
-			IGameSettingsReader gsr = new GameSettingsReader
-			{
-				SettingsDirectory = @"EscapeMinesInputs\"
-			};
+            IGameSettingsReader gsr = new GameSettingsReader
+            {
+                SettingsDirectory = @"EscapeMinesInputs\"
+            };
 
-			var gamesSettings = gsr.ReadSettings();
+            var gamesSettings = gsr.ReadSettings();
 
-			foreach (var gameSettings in gamesSettings)
-			{
-				// you can say that you did some benchmarking and eventually, File.ReadLines was faster.
-				var settings = File.ReadLines(gameSettings);
+            // if gamesSettings != null && gamesSettings.Any() { Print"No settings were found."}
 
-				IBoardGame em = new EscapeMines();
-				//em.Board.Length = 1;
-				// em.Board.Size = 4 5;
-				// em.Mines.Potition = 1,0 1,2 3,4;
-				// em.Exit.Potition = 1,0 1,2 3,4;
-				// em.Turtle.Potition = 1 0 N;
-				em.SetupBoard(settings);
+            foreach (var gameSettings in gamesSettings)
+            {
+                // you can say that you did some benchmarking and eventually, File.ReadLines was faster.
+                var settings = File.ReadLines(gameSettings);
 
-				//em
-				Console.WriteLine(settings.First());
-				Console.WriteLine(settings.Skip(1).First());
-				Console.WriteLine(settings.Skip(2).First());
-				Console.WriteLine(settings.Skip(3).First());
-				Console.WriteLine(settings.Skip(4).FirstOrDefault());
-				Console.WriteLine("\nNew Game Has Started.\n");
-				Console.ReadLine();
+                IBoardGame em = new EscapeMines();
+                //em.Board.Length = 1;
+                // em.Board.Size = 4 5;
+                // em.Mines.Potition = 1,0 1,2 3,4;
+                // em.Exit.Potition = 1,0 1,2 3,4;
+                // em.Turtle.Potition = 1 0 N;
+                em.SetupBoard(settings);
 
-				em.Play(); // em.Moves = R M L M M R M M M;
-				em.Result();
-				//em.End();
+                //em
+                Console.WriteLine(settings.First());
+                Console.WriteLine(settings.Skip(1).First());
+                Console.WriteLine(settings.Skip(2).First());
+                Console.WriteLine(settings.Skip(3).First());
+                Console.WriteLine(settings.Skip(4).FirstOrDefault());
+                Console.WriteLine("\nNew Game Has Started.\n");
+                Console.ReadLine();
 
-			}
-			Console.ReadKey();
+                em.Play(); // em.Moves = R M L M M R M M M;
+                em.Result();
+                //em.End();
+
+            }
+            Console.ReadKey();
 
 
 
-			//output
-			// either success or failure
-			// if the turtle does not reach the exit or doesn't hit a mine.
-		}
-	}
+            //output
+            // either success or failure
+            // if the turtle does not reach the exit or doesn't hit a mine.
+        }
+    }
 }
